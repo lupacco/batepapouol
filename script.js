89ecade75bc68350f76f7a2294a0e1c22f7f5958
Lucas <pagottolc@gmail.com>
2022-10-25 13:48:02 -0300

    interface de usuário v3

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
  * @brief UOL Chat - Lucas Pagotto C. Oliveira - 25/10/22
  *        Driven Turma 9 - Fourth week of the full-stack course
  */ 
+
+function setMenu(){
+    let menu = document.querySelector('.menu')
+    menu.classList.toggle('hide')
+}
