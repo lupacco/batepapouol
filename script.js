ec81b78049b229c09c746f7342a43db1ca3f3e32
lupacco <pagotolc@gmail.com>
2022-10-26 00:11:32 -0300

    before matheus

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * @brief UOL Chat - Lucas Pagotto C. Oliveira - 25/10/22
  *        Driven Turma 9 - Fourth week of the full-stack course
  */ 
-
+let visibility = document.querySelector('.public')
 function setMenu(){
     let menu = document.querySelector('.menu')
     menu.classList.toggle('hide')
@@ -11,4 +11,18 @@ function setMenu(){
 function checkOption(option){
     let icon = option.querySelector('.check')
     icon.classList.toggle('hide')
+}
+
+function setVisibility(option){
+    let checkIcon = option.querySelector('ion-icon')
+    if(checkIcon.name == 'lock-open'){
+        console.log('open')
+        checkOption(option)
+    } else{
+        console.log('closed')
+    }
+}
+
+function getThis(itme){
+    console.log(itme)
 }
