e65499675500023e7e880f2a717301482874b816
lupacco <pagotolc@gmail.com>
2022-10-27 19:23:28 -0300

    Login e renderização de chat OK, falta atualizar chat recorrentemente

--- script.js
+++ script.js
@@ -1,120 +1,10 @@
 /**
  * @brief UOL Chat - Lucas Pagotto C. Oliveira - 25/10/22
  *        Driven Turma 9 - Fourth week of the full-stack course
- */ 
+ */
 
 
-function sameMessage(lastMessage, currentMessage) {
-    if(lastMessage.from == currentMessage.from &&
-        lastMessage.to   == currentMessage.to   &&
-        lastMessage.text == currentMessage.text &&
-        lastMessage.type == currentMessage.type &&
-       lastMessage.time == currentMessage.time) {
-        return true;
-    }
-    return false;
-}
-
-function renderChat(){
-    console.log('atualizou')
-    axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
-    .then(response => {
-        console.log(response.data[99])
-        //response.data.forEach(msg => {
-            //     if(msg.type == 'message'){
-                //         document.querySelector('main').innerHTML += `
-                //             <div class="msg">
-                //                 <p>
-                //                     <span class="msgDate">
-                //                         (${(msg.time)})
-                //                     </span>
-                //                     <span class="user">
-                //                         <strong>${(msg.from)}</strong> para <strong>${(msg.to)}</strong>:
-                //                     </span>
-                //                     <span class="action">
-                //                         ${(msg.text)}
-                //                     </span>
-        //                 </p>
-        //             </div>
-        //         `
-        //     }
-        //     else if(msg.type == 'status'){
-            //         document.querySelector('main').innerHTML += `
-            //             <div class="msg">
-            //                 <p>
-            //                     <span class="msgDate">
-            //                         (${(msg.time)})
-            //                     </span>
-            //                     <span class="user">
-            //                         <strong>${(msg.from)}</strong>
-            //                     </span>
-            //                     <span class="action">
-            //                         ${(msg.text)}
-            //                     </span>
-            //                 </p>
-            //             </div>
-            //         `
-            //     }
-            //     else if(msg.type == 'private_message'){
-                //         document.querySelector('main').innerHTML += `
-                //             <div class="msg reserved">
-                //                 <p>
-                //                     <span class="msgDate">
-                //                         (${(msg.time)}) 
-        //                     </span>
-        //                     <span class="user">
-        //                         <strong>${(msg.from)}</strong> reservadamente para <strong>${(msg.to)}</strong>: 
-        //                     </span>
-        //                     <span class="action">
-        //                         ${(msg.text)}
-        //                     </span>
-        //                 </p>
-        //             </div>
-        //         `
-        //     }
-        //             //msg.scrollIntoView()     
-        // })
-        //console.log(lastMessage)
-    })
-    .catch(err => {
-        //console.log(err)
-    })
-}
-//setInterval(renderChat(),1000)
-
-function sendMessage(){
-    let messageToSend = document.querySelector('textarea').value
-    message.text = messageToSend
-    axios.post('https://mock-api.driven.com.br/api/v6/uol/messages',message)
-    .then(res => {
-        //console.log(res)
-    })
-    messageToSend = ''
-}
-
-const user = {}
-let message = {
-    from: user.name,
-    to: "Todos",
-    text: "Eu vou ser rei dos pitadas poha",
-    type: "message" 
-}
-function joinChat(){
-    user.name = prompt('Qual é o seu usuário?')
-    axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',user)
-    .then(() => {
-        renderChat()
-    })
-    
-}
-// joinChat()
-
-
-function setMenu(){
-    let menu = document.querySelector('.menu')
-    menu.classList.toggle('hidden')
-}
-
+//INTERFACE FUNCTIONS
 
 //Ensures that only one contact option is selected
 let contacts = Array.from(document.querySelectorAll('.contacts h3') )
@@ -136,4 +26,153 @@ privacy.forEach(item => {
         event.target.classList.toggle('hide')
     })    
 })
+
+//Global variable representing user an it attributes
+const user = {}
+
+//Set User's name
+function setUser(){
+    //get value of input
+    let inputValue = document.querySelector('.login input').value
+    //set user's name based on input value
+    user.name = inputValue
+    let loginScreen = document.querySelector('.login')
+    loginScreen.classList.add('hidden')
+    joinChat()
+}
+
+//Open menu sidebar
+function openMenu(){
+    let menu = document.querySelector('.menu')
+    menu.classList.toggle('hidden')
+}
+
+//API FUNCTIONS
+
+//Join chat with user's name
+function joinChat(){
+    axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',user)
+    .then(res => {
+        console.log(res)
+        console.log(`${user.name} entrou no chat`)
+        renderChat()
+    })
+    
+}
+// joinChat()
+function sameMessage(lastMessage, currentMessage) {
+    if(lastMessage.from == currentMessage.from &&
+        lastMessage.to   == currentMessage.to   &&
+        lastMessage.text == currentMessage.text &&
+        lastMessage.type == currentMessage.type &&
+        lastMessage.time == currentMessage.time) {
+            return true;
+        }
+    return false;
+}
+//Render single message
+function renderMessage(msg){
+    if(msg.type == 'message'){
+        document.querySelector('main').innerHTML += `
+            <div class="msg">
+                <p>
+                    <span class="msgDate">
+                        (${msg.time})
+                    </span>
+                    <span class="user">
+                        <strong>${msg.from}</strong> para <strong>${msg.to}</strong>:
+                    </span>
+                    <span class="action">
+                        ${(msg.text)}
+                    </span>
+        </p>
+    </div>
+    `
+    }
+    else if(msg.type == 'status'){
+            document.querySelector('main').innerHTML += `
+                <div class="msg">
+                    <p>
+                        <span class="msgDate">
+                            (${msg.time})
+                        </span>
+                        <span class="user">
+                            <strong>${msg.from}</strong>
+                        </span>
+                        <span class="action">
+                            ${msg.text}
+                        </span>
+                    </p>
+                </div>
+            `
+        }
+        else if(msg.type == 'private_message'){
+                document.querySelector('main').innerHTML += `
+                    <div class="msg reserved">
+                        <p>
+                            <span class="msgDate">
+                                (${msg.time}) 
+                    </span>
+                    <span class="user">
+                        <strong>${msg.from}</strong> reservadamente para <strong>${msg.to}</strong>: 
+                    </span>
+                    <span class="action">
+                        ${msg.text}
+                    </span>
+                </p>
+            </div>
+        `
+    }
+}
+
+//Render entire chat after login
+function renderChat(){
+    console.log('renderizou chat')
+    axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    .then(res => {
+        res.data.forEach(msg => {
+            renderMessage(msg)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
+    //setInterval(renderLastMessage, 1000)
+    renderLastMessage()
+}
+//Render last message
+function renderLastMessage(){
+    axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    .then(res => {
+        console.log(res.data)
+        let lastMsgAPI = res.data[res.data.length - 1]
+        let lastMsgHTML = document.querySelector('main:last-child')
+        if(lastMsgHTML == null || !sameMessage(lastMsgAPI, lastMsgHTML)){
+            console.log('nao é a mesma mensagem')
+            renderMessage(lastMsgAPI)
+        }
+    })
+}
+
+function sendMessage(){
+    let messageToSend = document.querySelector('textarea').value
+    message.text = messageToSend
+    axios.post('https://mock-api.driven.com.br/api/v6/uol/messages',message)
+    .then(res => {
+        //console.log(res)
+    })
+    messageToSend = ''
+}
+
+let message = {
+    from: user.name,
+    to: "Todos",
+    text: "Eu vou ser rei dos pitadas poha",
+    type: "message" 
+}
+
+
+
+
+
 
